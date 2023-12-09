38f30e848172f991ac4049fc5d618839d358f41e
Sheraz <sheraztariq2978@gmail.com>
2023-12-09 15:02:34 +0500

    Database Schema for Admin panel

---
+++ backend/models/userSchema.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    contacts: {
+      type: Number,
+    },
+    appointmentsmade: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Appointments",
+    },
+
+    isDisabled: {
+      type: Boolean,
+    },
+
+    medicines: [
+      {
+        desc: String,
+        disease: String,
+        date1: [Date],
+      },
+    ],
+    report: [
+      {
+        type: String,
+        img: String,
+        desc: String,
+        title: String,
+        date: Date.now,
+      },
+    ],
+    reports: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Users", userSchema);
