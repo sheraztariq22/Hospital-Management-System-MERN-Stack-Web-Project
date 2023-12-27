f9ddec7457307564107a4050b02dcfc2e9de128a
Sheraz <sheraztariq2978@gmail.com>
2023-12-27 15:10:30 +0500

    integration of update patient info

--- frontend/src/components/updatePatient/index.jsx
+++ frontend/src/components/updatePatient/index.jsx
@@ -5,62 +5,88 @@ import { FaAddressCard } from "react-icons/fa6";
 import { MdMeetingRoom } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import FormButton from "../formButton/index";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 const UpdatePatient = () => {
-  const [patientData, setPatientData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3000/api/v1/admin/patient/getAllPatients",
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
-        );
-        console.log(response.data);
-        setPatientData(response.data);
-      } catch (error) {
-        console.error("Error fetching patient data:", error);
-      }
-    };
+  const [patientData, setPatientData] = useState({
+    patientName: "",
+    _id: "",
+    patientRoomNumber: "",
+    patientContact: "",
+    patientAttendentName: "",
+  });
 
-    fetchData();
-  }, []);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPatientData((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(patientData);
+    axios
+      .put(
+        `http://localhost:3000/api/v1/admin/patient/updatePatient/${patientData._id}`,
+        patientData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        alert("Patient Updated Successfully");
+        setPatientData({});
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Patient Updation Failed");
+      });
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormInputs
           type="text"
           id="1"
+          name="patientName"
           placeholder="Patient Name (to be update)"
+          onChange={handleChange}
           iconUrl={<FaUser />}
         />
         <FormInputs
-          type="number"
           id="2"
           placeholder="Patient ID (to be update)"
+          name="_id"
+          onChange={handleChange}
           iconUrl={<FaAddressCard />}
         />
         <FormInputs
-          type="number"
           id="3"
+          name="patientRoomNumber"
           placeholder="Patient Room Number (to be update)"
+          onChange={handleChange}
           iconUrl={<MdMeetingRoom />}
         />
         <FormInputs
-          type="number"
           id="4"
           placeholder="Patient Phone Number (to be update)"
+          name="patientContact"
+          onChange={handleChange}
           iconUrl={<FaPhoneAlt />}
         />
         <FormInputs
           type="text"
           id="5"
           placeholder="Patient Attendant Name (to be update)"
+          name="patientAttendentName"
+          onChange={handleChange}
           iconUrl={<FaUser />}
         />
         <FormButton button="Update" />
